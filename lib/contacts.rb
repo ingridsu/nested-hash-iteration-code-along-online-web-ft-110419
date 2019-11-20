5175b52ecf1f4e8261bd69bfa2e5588554605f23
Ingrid sura <sura.ingrid@gmail.com>
2019-11-20 04:39:58 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
    contacts.each do |person, contact_details_hash|
      if person == "Freddy mercury"
        contact_details_hash.each do |attribute, data|
-        
-       puts "#{person}: #{data}"
+             binding.pry 
+       
       |data|.each do || delete_if.include?(["strawberry"])
           end
