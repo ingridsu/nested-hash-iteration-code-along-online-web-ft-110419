916a4e9815198440c2fd3ed0b732e1740176c877
Ingrid sura <sura.ingrid@gmail.com>
2019-11-20 04:06:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,5 +17,4 @@ require 'pry'
 def remove_strawberry(contacts)
    contacts.each do |person,data|
        puts "#{person}: #{data}"
-     [:favorite_ice_cream_flavors].delete_if 
-   |data|.include? ["strawberry"]
+            |data|.delete_if(["strawberry"]).include? ["strawberry"]
